8b4bef44a5db43fcca7c4407e086a14fcd74c583
ikkromm18 <muhammadikkrom749@gmail.com>
2025-09-20 14:14:56 +0700

    add a UI HistoryPage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,6 +11,41 @@ const bannerData = [
   { id: 3, url: require("../../assets/images/banner.png") },
 ]
 
+// Data dummy Riwayat
+const riwayatData = [
+  {
+    id: 1,
+    title: "Pindah Antar Kabupaten / Provinsi",
+    date: "10 September 2025, 10.00",
+    icon: "location-on",
+  },
+  {
+    id: 2,
+    title: "Permohonan Ijin Hajatan",
+    date: "08 September 2025, 14.30",
+    icon: "event",
+  },
+  {
+    id: 3,
+    title: "Surat Dispen Nikah",
+    date: "05 September 2025, 09.15",
+    icon: "favorite-border",
+  },
+  {
+    id: 4,
+    title: "Pindah Dalam Provinsi",
+    date: "03 September 2025, 11.45",
+    icon: "location-on",
+  },
+  {
+    id: 5,
+    title: "Pindah Luar Provinsi",
+    date: "01 September 2025, 16.20",
+    icon: "flight-takeoff",
+  },
+];
+
+
 
 export default function Index() {
 
@@ -63,22 +98,22 @@ export default function Index() {
               className="flex flex-row items-center justify-center h-24 rounded-2xl"
             >
               <View className="flex flex-col items-center justify-center">
-                <MaterialIcons name="notifications-none" size={32} color="#fff" />
+                <MaterialIcons name="location-on" size={32} color="#fff" />
                 <Text className='w-24 text-xs font-light text-center text-white'>Pindah Dalam Provinsi</Text>
               </View>
 
               <View className="flex flex-col items-center justify-center">
-                <MaterialIcons name="notifications-none" size={32} color="#fff" />
+                <MaterialIcons name="flight-takeoff" size={32} color="#fff" />
                 <Text className='w-24 text-xs font-light text-center text-white'>Pindah Luar Provinsi</Text>
               </View>
 
               <View className="flex flex-col items-center justify-center">
-                <MaterialIcons name="notifications-none" size={32} color="#fff" />
+                <MaterialIcons name="event" size={32} color="#fff" />
                 <Text className='w-24 text-xs font-light text-center text-white'>Permohonan Ijin Hajatan</Text>
               </View>
 
               <View className="flex flex-col items-center justify-center">
-                <MaterialIcons name="notifications-none" size={32} color="#fff" />
+                <MaterialIcons name="favorite-border" size={32} color="#fff" />
                 <Text className='w-24 text-xs font-light text-center text-white'>Surat Dispen Nikah</Text>
               </View>
 
@@ -86,86 +121,41 @@ export default function Index() {
           </View>
 
           {/* Riwayat */}
-          <View className='px-6 mt-6 bg-[#172E35] py-2'>
-            <Text className='text-xl font-medium text-white'>Riwayat Pengajuan</Text>
+          <View className="px-6 mt-6 bg-[#172E35] py-2">
+            <Text className="text-xl font-medium text-white">Riwayat Pengajuan</Text>
 
-            {/* Flex Riwayat (3 Data) */}
-            <View className='flex flex-col'>
+            <View className="flex flex-col">
+              {riwayatData.slice(-3).map((item) => (
+                <View
+                  key={item.id}
+                  className="flex flex-row items-center justify-between border-b border-b-gray-600"
+                >
+                  <View className="flex flex-row items-center justify-center py-4">
+                    <View className="bg-[#fff] rounded-full p-2">
+                      <MaterialIcons name={item.icon as any} size={35} color="#03BA9B" />
+                    </View>
 
-              {/* Flex Per Data */}
-              <View className='flex flex-row items-center justify-between border-b border-b-gray-600'>
-
-                <View className='flex flex-row items-center justify-center py-4'>
-
-                  <View className="bg-[#fff] rounded-full p-2">
-                    <MaterialIcons name="notifications-none" size={35} color="#03BA9B" />
+                    <View className="flex flex-col ml-3 max-w-40">
+                      <Text className="font-normal text-white text-normal">{item.title}</Text>
+                      <Text className="text-xs font-light text-white">{item.date}</Text>
+                    </View>
                   </View>
 
-                  <View className='flex flex-col ml-3 max-w-40'>
-                    <Text className='font-normal text-white text-normal'>Pindah Antar Kabupaten / Provinsi</Text>
-                    <Text className='text-xs font-light text-white'>10 September 2025, 10.00</Text>
-                  </View>
-
+                  <FontAwesome6 name="angle-right" size={16} color="white" />
                 </View>
-                <FontAwesome6 name="angle-right" size={16} color="white" />
-
-              </View>
-
-              <View className='flex flex-row items-center justify-between border-b border-b-gray-600'>
-
-                <View className='flex flex-row items-center justify-center py-4'>
-
-                  <View className="bg-[#fff] rounded-full p-2">
-                    <MaterialIcons name="notifications-none" size={35} color="#03BA9B" />
-                  </View>
-
-                  <View className='flex flex-col ml-3 max-w-40'>
-                    <Text className='font-normal text-white text-normal'>Pindah Antar Kabupaten / Provinsi</Text>
-                    <Text className='text-xs font-light text-white'>10 September 2025, 10.00</Text>
-                  </View>
-
-                </View>
-                <FontAwesome6 name="angle-right" size={16} color="white" />
-
-              </View>
-
-              <View className='flex flex-row items-center justify-between border-b border-b-gray-600'>
-
-                <View className='flex flex-row items-center justify-center py-4'>
-
-                  <View className="bg-[#fff] rounded-full p-2">
-                    <MaterialIcons name="notifications-none" size={35} color="#03BA9B" />
-                  </View>
-
-                  <View className='flex flex-col ml-3 max-w-40'>
-                    <Text className='font-normal text-white text-normal'>Pindah Antar Kabupaten / Provinsi</Text>
-                    <Text className='text-xs font-light text-white'>10 September 2025, 10.00</Text>
-                  </View>
-
-                </View>
-                <FontAwesome6 name="angle-right" size={16} color="white" />
-
-              </View>
-
-
-
-
-
-
-
+              ))}
             </View>
 
-            <View className='flex flex-row items-center justify-center gap-2 mt-2'>
-              <Text className='text-lg font-light text-center text-white'>Lainnya</Text>
+            <View className="flex flex-row items-center justify-center gap-2 mt-2">
+              <Text className="text-lg font-light text-center text-white">Lainnya</Text>
               <FontAwesome6 name="angle-right" size={10} color="white" />
             </View>
-
-
           </View>
 
 
+
           {/* Banner */}
-          <View className="mt-6">
+          <View className="pb-32 mt-6">
             <ScrollView
               horizontal
               showsHorizontalScrollIndicator={false}
